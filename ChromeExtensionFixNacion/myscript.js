bb0a7eea26907118670f210f0fb823ffdbc627db
wcubero <wcubero@hotmail.com>
2022-11-09 11:06:42 -0600

    update

--- ChromeExtensionFixNacion/myscript.js
+++ ChromeExtensionFixNacion/myscript.js
@@ -2,6 +2,10 @@
 
 try {
 
+	document.querySelector("#main > div > div:nth-child(2) > div > article").setAttribute("opacity", "1");
+	document.querySelector("#main > div > div:nth-child(2) > div > article").setAttribute("style", "height: 600px;");
+	document.querySelector("#main > div > div:nth-child(2) > div > article").setAttribute("style", "overflow-y:auto;max-height: 1200px;");
+	
 	let data_financiero = document.querySelector("#main > div > div:nth-child(2) > div > article").getInnerHTML();
 
 	$(function () {
@@ -10,15 +14,20 @@ try {
 
 			let href = location.href;
 						
-			if (href.includes("elfinancierocr")) {
+			if (href.includes("elfinancierocr")) {				
 				
-			        data_financiero = data_financiero.replaceAll("article", "regalo").replaceAll("default_", "regalo_");													
-				data_financiero = data_financiero.replace(/<[^>]*>?/gm, '');
-				document.querySelector("#main-nav > div.news-theme-navigation-container.news-theme-navigation-bar.logo-left.horizontal-links > div.nav-logo.nav-logo-left.nav-logo-show > a > img").setAttribute("style", " transform: rotate(-10deg);");				
+				//Financiero	  	 	 			 
+				console.log(data_financiero);				 
+				data_financiero = data_financiero.replaceAll("avatar", "");		
+				data_financiero = data_financiero.replaceAll("avatar-skeleton", "");						
+				data_financiero = data_financiero.replaceAll("react-loading-skeleton", "");				
+				data_financiero = data_financiero.replaceAll("opacity", "");		 
+				data_financiero = data_financiero.replaceAll("display", "");		
+				data_financiero = data_financiero.replaceAll("visibility", "");											
 				document.querySelector("#main > div > div:nth-child(2) > div > article").innerHTML = data_financiero;
-				
-				
-				//Financiero	  	 	 
+																
+				document.querySelector("#main-nav > div.news-theme-navigation-container.news-theme-navigation-bar.logo-left.horizontal-links > div.nav-logo.nav-logo-left.nav-logo-show > a > img").setAttribute("style", " transform: rotate(-10deg);");
+
 				document.querySelector(".tp-modal").setAttribute("style", "display:none;");
 				document.querySelector(".tp-backdrop").setAttribute("style", "display:none;");
 				document.querySelector("#main").setAttribute("style", "overflow-y:auto;max-height: 1200px;");
@@ -44,6 +53,10 @@ try {
 
 try {
 
+	document.querySelector("#main > div > div:nth-child(2) > div > article").setAttribute("opacity", "1");
+	document.querySelector("#main > div > div:nth-child(2) > div > article").setAttribute("style", "height: 600px;");
+	document.querySelector("#main > div > div:nth-child(2) > div > article").setAttribute("style", "overflow-y:auto;max-height: 1200px;");
+		    	
 	let documento_original = document.querySelector("#main > div > div:nth-child(2) > div > article").getInnerHTML();
 
 	$(function () {
@@ -53,12 +66,17 @@ try {
 			let href = location.href;
 
 			if (href.includes("nacion")) {
-				 
+			 
 				//Nacion
-				//console.log(documento_original);		
-				documento_original = documento_original.replaceAll("article", "regalo").replaceAll("default_", "regalo_");
-				documento_original = documento_original.replace(/<[^>]*>?/gm, '');
+				console.log(documento_original);				 
+				documento_original = documento_original.replaceAll("avatar", "");		
+				documento_original = documento_original.replaceAll("avatar-skeleton", "");						
+				documento_original = documento_original.replaceAll("react-loading-skeleton", "");				
+				documento_original = documento_original.replaceAll("opacity", "");		 
+				documento_original = documento_original.replaceAll("display", "");		
+				documento_original = documento_original.replaceAll("visibility", "");											
 				document.querySelector("#main > div > div:nth-child(2) > div > article").innerHTML = documento_original;
+																
 				document.querySelector("#main-nav > div.news-theme-navigation-container.news-theme-navigation-bar.logo-left.horizontal-links > div.nav-logo.nav-logo-left.nav-logo-show > a > img").setAttribute("style", " transform: rotate(-10deg);");
 
 				document.querySelector(".tp-modal").setAttribute("style", "display:none;");
